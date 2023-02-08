e94c240f264b32a5ee32e25dbd20cbd9a21b78f3
Julius Pedersen <deifyed@tutanota.com>
2023-02-08 15:20:45 +0100

    ✅ Add basic home page design

--- src/home/HomeView.vue
+++ src/home/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="Home">
-    <p>Tuuturu is an ambition.</p>
+    <div class="item item-left">
+      <p>Tuuturu is an ambition.</p>
+    </div>
+    <div class="item item-right">
+      <p>It is a dream.</p>
+    </div>
   </div>
 </template>
 
@@ -13,4 +18,38 @@ export default defineComponent({
 })
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+$gap: 250px;
+
+.Home {
+  margin: 0 8px;
+  margin-top: $gap;
+  height: 100%;
+
+  display: grid;
+  grid-column: 800px 800px;
+  grid-row: 800px 800px;
+  grid-row-gap: $gap;
+}
+
+.item {
+  min-height: 400px;
+
+  background-color: #f5f5f5;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem 0.1rem #f5f5f5;
+}
+
+.item-left {
+  grid-column: 1 / 2;
+  grid-column-start: 1;
+  grid-row: 1;
+}
+
+.item-right {
+  grid-column: 2 / 3;
+  grid-row-start: 2;
+  grid-row: 2;
+}
+</style>
