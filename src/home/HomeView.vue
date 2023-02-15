6895f39b40426644b7d6e7a1063e431e4c462cb9
Julius Pedersen <deifyed@tutanota.com>
2023-02-15 19:06:03 +0100

    👌 Improve homeview design

--- src/home/HomeView.vue
+++ src/home/HomeView.vue
@@ -1,34 +1,36 @@
 <template>
   <div class="Home">
-    <div class="item">
-      <p class="bold">Tuuturu is ambition</p>
-      <p>
-        We strive for something better. Something more. More in terms of human
-        development. We've been blinded by technology instead of using
-        technology to improve core human mechanisms like democracy, education,
-        and health.
-      </p>
-    </div>
+    <div class="container">
+      <div class="item">
+        <p class="bold">Tuuturu is ambition</p>
+        <p>
+          We strive for something better. Something more. More in terms of human
+          development. We've been blinded by technology instead of using
+          technology to improve core human mechanisms like democracy, education,
+          and health.
+        </p>
+      </div>
 
-    <div class="item">
-      <p class="bold">Tuuturu is dreaming</p>
-      <p>
-        Dreaming of accountability and transparency. When politicians makes
-        promises, it should be easy to prove or disprove their claims. Even if
-        the claims are abstract and hard to measure, it should be possible to
-        track progress. If the community makes a change, it should be easy to
-        see the consequences.
-      </p>
-    </div>
+      <div class="item">
+        <p class="bold">Tuuturu is dreaming</p>
+        <p>
+          Dreaming of accountability and transparency. When politicians makes
+          promises, it should be easy to prove or disprove their claims. Even if
+          the claims are abstract and hard to measure, it should be possible to
+          track progress. If the community makes a change, it should be easy to
+          see the consequences.
+        </p>
+      </div>
 
-    <div class="item">
-      <p class="bold">Tuuturu is hope</p>
-      <p>
-        Too many choices are made based on immediate gratification, be it this
-        year's budget, the current election period or to soothe the loud
-        minority. Let's give voices to the silent and unavoidable data that can
-        silence even the strongest rhetoricians.
-      </p>
+      <div class="item">
+        <p class="bold">Tuuturu is hope</p>
+        <p>
+          Too many choices are made based on immediate gratification, be it this
+          year's budget, the current election period or to soothe the loud
+          minority. Let's give voices to the silent and unavoidable data that
+          can silence even the strongest rhetoricians.
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -48,17 +50,21 @@ p.bold {
 }
 
 .Home {
+  display: flex;
+  justify-content: center;
+}
+
+.container {
   margin-top: 8rem;
   height: 100%;
+  width: 60%;
 
   display: flex;
   flex-direction: column;
-
-  padding: 0 10rem;
 }
 
 .item {
-  width: 70%;
+  width: 80%;
 
   background-color: #f5f5f5;
   padding: 4rem;
@@ -79,8 +85,9 @@ p.bold {
 }
 
 @media only screen and (max-width: 768px) {
-  .Home {
+  .container {
     margin-top: 0.5rem;
+    width: 90%;
     padding: 0;
   }
 
